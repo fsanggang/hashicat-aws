51ed7d0109ca2ac9b81f28a1c7c1bf5b1ca6a29a
Fiona Sanggang <fiona.sanggang@sharesight.co.nz>
2023-02-22 03:54:20 +0000

    Remove bucket, it conflicts with bucket_prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/fsanggang/s3-bucket/aws"
   version = "2.8.0"
-
-  bucket = "hashicat-aws-bucket"
   bucket_prefix = "fiona-sharesight"
   acl    = "private"
 
